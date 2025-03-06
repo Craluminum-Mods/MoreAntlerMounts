21a75c486267ecf014357243944652db55e33d0a
Dana <beware.14b@gmail.com>
2025-03-06 16:14:43 +0200

    Add project files.

---
+++ MoreAntlerMounts/Systems/Core.cs
@@ -0,0 +1,11 @@
+﻿using Vintagestory.API.Common;
+
+namespace MoreAntlerMounts;
+
+public class Core : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        Mod.Logger.Event("started '{0}' mod", Mod.Info.Name);
+    }
+}
